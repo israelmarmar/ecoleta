74de548e6938456580690775fc47b5b1469c1bf3
israel martins <email.israel.br@gmail.com>
2020-06-03 13:58:37 -0400

    first commit

---
+++ server/README.md
@@ -0,0 +1,17 @@
+# Ecoleta Server API
+
+![alt text](https://github.com/marciocamello/nlw-ecoleta-server/blob/master/capa.jpeg)
+
+:rocket: Technologies
+
+- NodeJS
+- ExpressJS
+- Typescript
+- Knex  
+- JWT
+
+:blue_book: Refs
+
+- https://www.typescriptlang.org/docs/home/
+- https://knexjs.org/
+- https://expressjs.com/en/4x/api.html
---
+++ server/src/server.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import routes from './routes'
+import cors from 'cors'
+import path from 'path'
+import './config'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+
+const port = process.env.PORT || 3333
+
+app.listen(port, () => {
+  console.log(`Start Ecoleta API on port ${port}`)
+})
