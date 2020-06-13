81741dcdc3ce1957e3bdbf237d53eae64289cf0e
israel martins <email.israel.br@gmail.com>
2020-06-13 16:54:59 -0400

    fix bugs

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -1,15 +1,30 @@
 const path = require('path')
 
 module.exports = {
-  client: 'sqlite3',
-  connection: {
-    filename: path.resolve(__dirname, "dist", "database","database.sqlite")
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.resolve(__dirname, "dist", "database", "database.sqlite")
+    },
+    migrations: {
+      directory: path.resolve(__dirname, 'dist', 'database', 'migrations')
+    },
+    seeds: {
+      directory: path.resolve(__dirname, 'dist', 'database', 'seeds')
+    },
+    useNullAsDefault: true
   },
-  migrations: {
-    directory: path.resolve(__dirname, 'dist', 'database', 'migrations')
-  },
-  seeds: {
-    directory: path.resolve(__dirname, 'dist', 'database', 'seeds')
-  },
-  useNullAsDefault: true
+  production: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.resolve(__dirname, "dist", "database", "database.sqlite")
+    },
+    migrations: {
+      directory: path.resolve(__dirname, 'dist', 'database', 'migrations')
+    },
+    seeds: {
+      directory: path.resolve(__dirname, 'dist', 'database', 'seeds')
+    },
+    useNullAsDefault: true
+  }
 }
