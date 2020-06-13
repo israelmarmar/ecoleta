2e7913c9f0cc88bf7a71b0b97ced1a24ef0b007f
israel martins <email.israel.br@gmail.com>
2020-06-13 18:35:18 -0400

    fix DB connection

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -16,9 +16,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: {
-      connection: process.env.DATABASE_URL,
-    },
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: path.resolve(__dirname, 'dist', 'database', 'migrations')
     },
