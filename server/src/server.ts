39e939b7eb703f9df33a37536756f236ba9b19d4
israel martins <email.israel.br@gmail.com>
2020-06-13 20:23:41 -0400

    fix form

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,12 @@ import { errors } from 'celebrate';
 
 const app = express();
 
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
 app.use(cors());
 app.use(express.json());
 app.use(routes);
